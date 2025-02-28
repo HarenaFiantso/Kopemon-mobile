130c3cdaae71ff9f9241079cb203db3daace084d
FiantsoHarena <fiantsorav@gmail.com>
2025-02-28 08:13:41 +0300

    chore: screen orientation handler

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -2,9 +2,19 @@ import { darkTheme, lightTheme } from '@/theme/theme'
 import { Stack } from 'expo-router'
 import { useColorScheme } from 'react-native'
 import { ThemeProvider } from 'styled-components'
+import * as ScreenOrientation from 'expo-screen-orientation'
+import { useEffect } from 'react'
 
 export default function RootLayout() {
   const scheme = useColorScheme()
+
+  useEffect(() => {
+    const unlockScreenOerientation = async () => {
+      await ScreenOrientation.unlockAsync()
+    }
+    unlockScreenOerientation()
+  }, [])
+
   return (
     <ThemeProvider theme={scheme === 'dark' ? darkTheme : lightTheme}>
       <Stack screenOptions={{ headerShown: false }} />
--- theme/theme.ts
+++ theme/theme.ts
@@ -1,11 +1,11 @@
 export const lightTheme = {
   background: '#FFFFFF',
   text: '#000000',
-  lightGray: '#e0e0e0'
+  lightGray: '#e0e0e0',
 }
 
 export const darkTheme = {
   background: '#2b2b2b',
   text: '#FFFFFF',
-  lightGray: '#e0e0e0'
+  lightGray: '#e0e0e0',
 }
